c08b6035e72d05f43aad858b9eedef2a06707437
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-02-04 16:41:52 -0800

    update: list of tuples is functioning

--- histogram.py
+++ histogram.py
@@ -23,31 +23,18 @@ text = "one fish two fish red fish blue fish"
 file = "text_file.txt"
 
 def histogram(source_text):
-    '''
     # return stored data structured of each unique words
     dictionary = dict()
     with open('{}'.format(source_text), 'r') as f:
         text_file = f.read()
-        text = text_file.split()
-
-    '''
-    text = source_text.split(' ')
-     = []
-    sum_list = []
+        text = text_file.split(' ')
 
     for word in text:
-        is_found = False
-        for w in sum_list:
-            if w[0] == word:
-                is_found = True
-                w[1] = w[1] + 1
-                break
-        if not is_found:
-            sum_list.append([word, 1])
-
-
-    print(sum_list)
-
+        if word not in dictionary:
+            dictionary[word] = 1
+        else:
+            dictionary[word] += 1
+    return dictionary
 
 
 def unique_words(histogram):
@@ -70,12 +57,54 @@ def frequency(word, histogram):
         print("Not Found")
 
 
+def list_of_list(source_text):
+    text = source_text.split(' ')
+    list_of_list = []
+    sum_list = []
+
+    for word in text:
+        found = False
+        for w in sum_list:
+            if w[0] == word:
+                found = True
+                w[1] = w[1] + 1
+                break
+        if not found:
+            sum_list.append([word, 1])
+
+
+    print(sum_list)
+
+
+def list_of_tuples(source_text):
+    text = source_text.split(' ')
+    list = []
+
+    for word in text:
+        found = False
+        for w in list:
+            if word == w[0]:
+                found = True
+                count = w[1] + 1
+                list.remove(w)
+                list.append((word, count))
+                break
+
+        if not found:
+            list.append((word, 1))
+    # tuple = list(zip(unique_words, frequency))
+
+    print(list)
+
+
+
 def main():
-    histo = histogram(text)
+    # histo = histogram(file)
     # unique_words(histo)
     # word = sys.argv[1]
     # frequency(word, histo)
-
+    # list_of_list(text)
+    list_of_tuples(text)
 
 if __name__ == "__main__":
     main()
