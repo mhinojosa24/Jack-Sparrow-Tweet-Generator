dfbde329cf68e9b3d02f8d166b62ff76730179f3
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-03-04 19:34:19 -0800

    heroku hell conflict

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-from flask import Flask, render_template
-# from stochastic_sampling import *
-
-import sys
-app = Flask(__name__)
-
-
-@app.route('/')
-def main():
-    # return generate_sentence()
-    return render_template('index.html')
-
-#tempo comment
-# def generate_sentence():
-#     file = 'sample_corpus.txt'
-#     random_words = randomized_word(file)
-#     return random_words
-# from flask import Flask
+# from flask import Flask, render_template
+# # from stochastic_sampling import *
+#
+# import sys
 # app = Flask(__name__)
 #
+#
 # @app.route('/')
-# def hello_world():
-#     return 'Hello, Worlddd!'
+# def main():
+#     # return generate_sentence()
+#     return render_template('index.html')
+
+#tempo comment
+def generate_sentence():
+    file = 'sample_corpus.txt'
+    random_words = randomized_word(file)
+    return random_words
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Worlddd!'
