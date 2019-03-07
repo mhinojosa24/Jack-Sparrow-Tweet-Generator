698d46507530dd60bed4ddb382ca8f7723f76a21
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-03-07 12:07:49 -0800

    [add] new function

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-from flask import Flask, render_template
-# # from stochastic_sampling import *
+# from flask import Flask, render_template
+# # # from stochastic_sampling import *
+# #
+# # import sys
+# app = Flask(__name__)
 #
-# import sys
-app = Flask(__name__)
 #
+# @app.route('/')
+# def main():
+#     return 'hi'
+#     # return generate_sentence()
+#     # return render_template('index.html')
 #
-@app.route('/')
-def main():
-    return 'hi'
-    # return generate_sentence()
-    # return render_template('index.html')
-
-#tempo comment
-def generate_sentence():
-    file = 'sample_corpus.txt'
-    random_words = randomized_word(file)
-    return random_words
+# #tempo comment
 from flask import Flask
+from stochastic_sampling import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Worlddd!'
+
+@app.route('/generate-words')
+def generate_sentence():
+    file = 'sample_corpus.txt'
+    random_words = randomized_word(file)
+    return random_words
