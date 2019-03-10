22d92f995d22d6437a9c58f5f221e67731a96868
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-03-09 16:18:09 -0800

    [add] rendering template for tweet gen

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from markov_chain import *
 app = Flask(__name__)
 
--- markov_chain.py
+++ markov_chain.py
@@ -1,7 +1,7 @@
 # from nltk.corpus import words
 from stochastic_sampling import *
 from dictogram import Dictogram
-import random
+import random, re
 
 
 
@@ -10,9 +10,11 @@ def markov_chain(list_of_values):
     mc = Dictogram()
 
     i = 0
-    while i < len(list_of_values) - 2:
-        pair = (list_of_values[i], list_of_values[i+1]) # pair of words in tuple
-        next_word = list_of_values[i+2] # next word
+    while i < len(list_of_values) - 5:
+        pair = (list_of_values[i], list_of_values[i+1], list_of_values[i+2], list_of_values[i+3], list_of_values[i+5]) # pair of words in tuple
+        # print(list_of_values[i], list_of_values[i+1], list_of_values[i+2], list_of_values[i+3], list_of_values[i+5])
+        next_word = list_of_values[i+5] # next word
+
         if pair in mc: # check to see if pair of words exist in the dictionary
             mc[pair].add_count(next_word) # increments the count of the next word
 
@@ -21,7 +23,7 @@ def markov_chain(list_of_values):
             new_dict.add_count(next_word) # add count of next word
             mc[pair] = new_dict #
         i += 1
-    print('markov chain ==> \n', mc, '\n<== end of markov chain\n')
+    # print('markov chain ==> \n', mc, '\n<== end of markov chain\n')
     return mc
 
 
@@ -39,10 +41,13 @@ def generate_sentence(dictogram, num_words):
 
         words_for_sent.append(random_set[0])
         words_for_sent.append(random_set[1])
+        words_for_sent.append(random_set[2])
+        words_for_sent.append(random_set[3])
+        words_for_sent.append(random_set[4])
 
         get_next_set = dictogram[random_set]
-        # print("next set of tuple ===> ", get_next_set)
-    print('words for sentence ==> ', words_for_sent, '\n<== end\n')
+        # print('next set of tuple ===> ', get_next_set)
+    # print('words for sentence ==> ', words_for_sent, '\n<== end\n')
 
     for word in words_for_sent:
         new_sentence += word + " "
@@ -50,35 +55,17 @@ def generate_sentence(dictogram, num_words):
 
 
 
-
-
-
-
-
-
-    # for i in range(num_words):
-    #     tuple_key =  tuple(words_for_sent[-2:])
-    #
-    #     value = dictogram[tuple_key]
-    #     sampler_word = randomized_word(value)
-    #     words_for_sent.append(sampler_word)
-    #
-    #
-    #     for word in  words_for_sent:
-    #
-    #         new_sentence += word + " "
-    #
-    # return new_sentence
-    #
-
-
 def main():
-    sample = 'I like cats. I love dogs. I hate nasty food. Although I love love food.'
-    sample = sample.split(" ")
-    dict = markov_chain(sample)
-    # print(dict)
-    gen = generate_sentence(dict, 10)
-    print(gen)
+    sample = 'I like cats. I love dogs. I hate mr.max nasty food. Although I love love food.'
+    with open( 'text_file.txt', "r") as f:
+        data = f.read()
+        # words_list = data.split()
+        content = re.sub('[^a-zA-Z0-9 \n\.]', '', data)
+        sample = data.split(' ')
+        dict = markov_chain(sample)
+        # print(dict)
+        gen = generate_sentence(dict, 4)
+        print(gen)
 
 
 if __name__ == '__main__':
