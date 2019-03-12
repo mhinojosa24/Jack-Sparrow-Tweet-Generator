c238ac7354d5751551d5877418d21418cc96adc2
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-03-11 22:11:37 -0700

    [fixed] text alignment

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask import request
 from markov_chain import *
-from flask import render_template
+import sys
+# import twitter
 
 
 
@@ -10,8 +12,7 @@ app = Flask(__name__, template_folder='templates', static_folder='css')
 
 with open('text_file.txt', "r") as f:
     data = f.read()
-    # data = data.decode("utf-8", "replace")
-    # data = str(data, 'utf-8')
+    data = data.decode("utf-8", "replace")
     words_list = data.split()
 
 
@@ -21,5 +22,11 @@ def generator():
     sentence = generate_sentence(markov, 10)
     return render_template('index.html', sentence=sentence)
 
+# @app.route('/tweet', methods=['POST'])
+# def tweet():
+#     status = request.form['sentence']
+#     twitter.tweet(status)
+#     return redirect('/')
+
 if __name__ == '__main__':
-    app.run(debug=True) #
+    app.run(debug=True)
