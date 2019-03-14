2427f846aa56ff0f39a32ba147cd5a32adc2481e
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-03-13 18:01:20 -0700

    [add] tweet generator is fully functioning

--- markov_chain.py
+++ markov_chain.py
@@ -2,7 +2,7 @@
 from class_methods.stochastic_sampling import *
 # from stochastic_sampling import *
 from class_methods.dictogram import Dictogram
-from dictogram import Dictogram
+# from dictogram import Dictogram
 import random, re
 
 
