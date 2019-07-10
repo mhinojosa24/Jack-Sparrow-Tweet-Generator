82cacfbe28f40273dca0d45bb48f864d472b9902
max <maximohinojosa24@yahoo.com>
2019-07-10 01:40:22 -0700

    [add] heroku

--- twitter.py
+++ twitter.py
@@ -1,6 +1,7 @@
 import os
 import dotenv
 
+
 # dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env")) # Of course, replace by your correct path
 # os.environ.update(dotenv)
 from requests_oauthlib import OAuth1Session
@@ -11,7 +12,7 @@ consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
 consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
 access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
 access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
-  
+
 
 session = OAuth1Session(consumer_key,
                         client_secret=consumer_secret,
